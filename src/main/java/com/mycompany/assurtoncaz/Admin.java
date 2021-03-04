d8bee7a4287d864d09f4e85b820ca153384eb6a9
soupapoule <62351063+soupapoule@users.noreply.github.com>
2021-03-04 14:29:41 +0400

    first com

---
+++ src/main/java/com/mycompany/assurtoncaz/Admin.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.assurtoncaz;
+
+/**
+ *
+ * @author manu
+ */
+public class Admin {
+    private int id;
+    private String Username;
+    private String Password;
+
+    public int getId() {
+        return id;
+    }
+
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String Username) {
+        this.Username = Username;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String Password) {
+        this.Password = Password;
+    }
+    
+ public static void main(String[] args) {
+       
+      
+        
+        
+        
+         System.out.println("Coucou le monde");
+
+    }
+}
